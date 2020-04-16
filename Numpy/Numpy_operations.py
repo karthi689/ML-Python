99ebad82a11c3951c7b3940fd48bbef366dcc0c6
Karthik Mummidisetti <karthik.mummidisetti@sparksoftcorp.com>
2020-04-16 18:12:25 -0400

    Random number generation

--- Numpy/Numpy_operations.py
+++ Numpy/Numpy_operations.py
@@ -48,3 +48,42 @@ print("Every 5th number from 50 in reverse order\n",np.arange(50,-1,-5))
 
 print("21 linearly spaced numbers between 1 and 5\n--------------------------------------------")
 print(np.linspace(1,5,21))
+
+
+""" 
+Zeroes, Ones, empty, and Identity matrix
+"""
+
+print("Vector of zeroes\n---------------------")
+print(np.zeros(5))
+print("Matrix of zeroes\n--------------------")
+print(np.zeros((3,4))) # Notice Tuples
+
+print("Vector of ones\n---------------------")
+print(np.ones(5))
+print("Matrix of ones\n---------------------")
+print(np.ones((5,2))) # Note matrix dimension specified by Tuples
+print("Matrix of 5's\n---------------------")
+print(5*np.ones((3,5)))
+
+print("Empty matrix\n-------------\n", np.empty((3,5)))
+
+mat1 = np.eye(4) 
+print("Identity matrix of dimension", mat1.shape)
+print(mat1)
+
+
+""" 
+Random number generation
+"""
+
+print("Random number generation (from Uniform distribution)")
+print(np.random.rand(2,3)) # 2 by 3 matrix with random numbers ranging from 0 to 1, Note no Tuple is necessary
+
+print("Numbers from Normal distribution with zero mean and standard deviation 1 i.e. standard normal")
+print(np.random.randn(4,3))
+
+print("Random integer vector:",np.random.randint(1,100,10)) #randint (low, high, # of samples to be drawn)
+print ("\nRandom integer matrix")
+print(np.random.randint(1,100,(4,4))) #randint (low, high, # of samples to be drawn in a tuple to form a matrix)
+print("\n20 samples drawn from a dice throw:",np.random.randint(1,7,20)) # 20 samples drawn from a dice throw
